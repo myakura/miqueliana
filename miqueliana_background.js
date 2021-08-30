59560d828fa342fe6534bd83be9a643b92de6414
Masataka Yakura <masataka.yakura@gmail.com>
2021-08-31 00:11:47 +0900

    introduce flashBadge()

--- miqueliana_background.js
+++ miqueliana_background.js
@@ -1,5 +1,22 @@
 console.log("hello");
 
+function flashBadge({ success = true }) {
+	// credit: https://github.com/chitsaou/copy-as-markdown
+	const text = success ? `✔` : `✘`;
+	const color = success ? `hsl(135deg 70% 30%)` : `hsl(0deg 80% 40%)`;
+	const emptyText = ``;
+	const transparent = `#000f`;
+	const timeout = 1000;
+
+	chrome.browserAction.setBadgeText({ text: text });
+	chrome.browserAction.setBadgeBackgroundColor({ color: color });
+
+	setTimeout(() => {
+		chrome.browserAction.setBadgeText({ text: emptyText });
+		chrome.browserAction.setBadgeBackgroundColor({ color: transparent });
+	}, timeout);
+}
+
 function getCurrentTab() {
 	return new Promise((resolve, reject) => {
 		chrome.tabs.query({ currentWindow: true, highlighted: true }, (tabs) => {
@@ -17,6 +34,7 @@ async function sendCommand(commandName) {
 		chrome.tabs.sendMessage(tab.id, { message: commandName });
 	} catch (error) {
 		console.error(error);
+		flashBadge({ success: false });
 	}
 }
 
