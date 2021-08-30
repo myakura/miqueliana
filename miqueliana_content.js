47bb9e3175a22f7c425cba7f925be39451cc5976
Masataka Yakura <masataka.yakura@gmail.com>
2021-08-31 01:08:21 +0900

    add getSelectionFragment() to content script

--- miqueliana_content.js
+++ miqueliana_content.js
@@ -1,7 +1,17 @@
 console.log("hello");
 
+function getSelectionFragment() {
+	// TODO: handle empty selections
+	const selection = window.getSelection();
+	const range = selection.getRangeAt(0);
+	return range.cloneContents();
+}
+
 function handleMessage(message) {
 	console.log(message);
+
+	const fragment = getSelectionFragment();
+	console.log(fragment);
 }
 
 chrome.runtime.onMessage.addListener(handleMessage);
