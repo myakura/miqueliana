1c98aa60c25a36f7486b8f782428c772758655b7
Masataka Yakura <masataka.yakura@gmail.com>
2021-08-30 19:42:37 +0900

    send a message from background to content when clicking on browser action

--- miqueliana_background.js
+++ miqueliana_background.js
@@ -2,6 +2,11 @@ console.log("hello");
 
 function handleActionClick() {
 	console.log("action clicked.");
+
+	chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+		console.log(tabs);
+		chrome.tabs.sendMessage(tabs[0].id, { message: "hello from bakground script." })
+	})
 }
 
 chrome.browserAction.onClicked.addListener(handleActionClick);
--- miqueliana_content.js
+++ miqueliana_content.js
@@ -1,1 +1,7 @@
 console.log("hello");
+
+function handleMessage(message) {
+	console.log(message);
+}
+
+chrome.runtime.onMessage.addListener(handleMessage);
