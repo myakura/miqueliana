9d5824d77a3b99328804c576cbed292e8089403c
Masataka Yakura <masataka.yakura@gmail.com>
2021-09-01 03:12:06 +0900

    add functions to walk fragment node tree

--- miqueliana_content.js
+++ miqueliana_content.js
@@ -9,11 +9,34 @@ function getSelectionFragment() {
 	return range.cloneContents();
 }
 
+function createFragmentWalker(fragment) {
+	if (!fragment) {
+		return null;
+	}
+	const whatToShow = Node.ELEMENT_NODE + Node.TEXT_NODE;
+	const treeWalker = document.createTreeWalker(fragment, whatToShow);
+	return treeWalker;
+}
+
+function walkTree(treeWalker) {
+	if (!treeWalker) {
+		return null;
+	}
+	let currentNode = treeWalker.firstChild();
+	while (currentNode) {
+		console.log(currentNode);
+		currentNode = treeWalker.nextNode();
+	}
+}
+
 function handleMessage(message) {
 	console.log(message);
 
 	const fragment = getSelectionFragment();
 	console.log(fragment);
+
+	const treeWalker = createFragmentWalker(fragment);
+	walkTree(treeWalker);
 }
 
 chrome.runtime.onMessage.addListener(handleMessage);
